7dcbbfcaf765eea030b15e1b9cf03f6f02e08a26
#YUHAS MICHAEL JOHN# <michaelj@asp2a-login-ntu02.head.cm.asp2a.nscc.sg>
2024-05-02 17:48:22 +0800

    Adding anytime capabilities for ENet

--- model/lanenet/LaneNet.py
+++ model/lanenet/LaneNet.py
@@ -9,14 +9,14 @@ import torch.nn.functional as F
 
 from model.lanenet.loss import DiscriminativeLoss
 from model.lanenet.backbone.UNet import UNet_Encoder, UNet_Decoder
-from model.lanenet.backbone.ENet import ENet_Encoder, ENet_Decoder
+from model.lanenet.backbone.AnytimeENet import *
 from model.lanenet.backbone.deeplabv3_plus.deeplabv3plus import Deeplabv3plus_Encoder, Deeplabv3plus_Decoder
 
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
-class LaneNet(nn.Module):
-    def __init__(self, in_ch = 3, arch="ENet"):
+class LaneNet(pytorch_lightning.LightningModule):
+    def __init__(self, in_ch = 3, arch="ENet", base_model=""):
         super(LaneNet, self).__init__()
         # no of instances for segmentation
         self.no_of_instances = 3  # if you want to output RGB instance map, it should be 3.
@@ -46,6 +46,35 @@ class LaneNet(nn.Module):
             self._decoder_instance = Deeplabv3plus_Decoder(self.no_of_instances)
             self._decoder_binary.to(DEVICE)
             self._decoder_instance.to(DEVICE)
+        elif self._arch == 'ENetBlock2':
+            m = LaneNet()
+            m.load_state_dict(torch.load('log/base_model.pth'))
+            self._encoder = ENet_EncoderBlock2(m._encoder)
+            self._encoder.to(DEVICE)
+            self._decoder_binary = ENet_DecoderEarlyExit(scale_factor=8, in_ch=128, out_ch=2)
+            self._decoder_binary.to(DEVICE)
+
+            self._decoder_instance = ENet_DecoderEarlyExit(scale_factor=8, in_ch=128, out_ch=self.no_of_instances)
+            self._decoder_instance.to(DEVICE)
+        elif self._arch == 'ENetBlock1':
+            m = LaneNet()
+            m.load_state_dict(torch.load('log/base_model.pth'))
+            self._encoder = ENet_EncoderBlock1(m._encoder)
+            self._encoder.to(DEVICE)
+            self._decoder_binary = ENet_DecoderEarlyExit(scale_factor=4, in_ch=64, out_ch=2)
+
+            self._decoder_instance = ENet_DecoderEarlyExit(scale_factor=4, in_ch=64, out_ch=self.no_of_instances)
+            self._decoder_instance.to(DEVICE)
+        elif self._arch == 'ENetBlock0':
+            m = LaneNet()
+            m.load_state_dict(torch.load('log/base_model.pth'))
+            self._encoder = ENet_EncoderBlock0(m._encoder)
+            self._encoder.to(DEVICE)
+            self._decoder_binary = ENet_DecoderEarlyExit(scale_factor=2, in_ch=16, out_ch=2)
+            self._decoder_binary.to(DEVICE)
+
+            self._decoder_instance = ENet_DecoderEarlyExit(scaler_factor=2, in_ch=16, out_ch=self.no_of_instances)
+            self._decoder_instance.to(DEVICE)
         else:
             raise("Please select right model.")
 
@@ -57,9 +86,11 @@ class LaneNet(nn.Module):
             c1, c2, c3, c4, c5 = self._encoder(input_tensor)
             binary = self._decoder_binary(c1, c2, c3, c4, c5)
             instance = self._decoder_instance(c1, c2, c3, c4, c5)
-        elif self._arch == 'ENet':
+        elif self._arch.startswith('ENet'):
             c = self._encoder(input_tensor)
             binary = self._decoder_binary(c)
+            # I think this is required
+            #binary = self.sigmoid(binary)
             instance = self._decoder_instance(c)
         elif self._arch == 'DeepLabv3+':
             c1, c2 = self._encoder(input_tensor)
